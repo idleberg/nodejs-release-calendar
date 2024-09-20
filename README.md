5bcd06c8afe115477fc4555c6cdece140444b70e
Jan T. Sott <git@idleberg.com>
2024-09-20 10:40:15 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# 📆 Node.js Release Calendar
+
+> Node.js Release Calendar for Apple Calendar, Outlook etc.
+
+[![Build](https://img.shields.io/github/actions/workflow/status/idleberg/nodejs-release-calendar/gh-pages.yml?style=for-the-badge)](https://github.com/idleberg/nodejs-release-calendar/actions)
+
+Subscribe to this URL:
+
+```
+https://idleberg.github.io/nodejs-release-calendar/node-releases.ics
+```
+
+## Self-hosted
+
+In order to host the feed yourself, please follow these steps:
+
+```sh
+# fork repository
+git clone https://github.com/idleberg/nodejs-release-calendar
+
+# install dependencies
+cd nodejs-release-calendar && bun install
+
+# build feeds
+bun run build
+```
+
+You can now deploy the files inside the `public`-folder to your webspace.
+
+## Related
+
+[Node.js Releases](https://github.com/nodejs/release)
---
+++ bin/build.js
@@ -0,0 +1,94 @@
+import { createEvents } from "ics";
+import { minify as htmlMinify } from "html-minifier-terser";
+import { promises as fs } from "node:fs";
+import { render } from "ejs";
+import { resolve } from "node:path";
+
+const outputFile = 'nodejs-releases.ics';
+
+const htmlMinifyOptions = {
+	collapseWhitespace: true,
+	removeAttributeQuotes: true,
+	removeComments: true,
+};
+
+await main();
+
+async function main() {
+	const schedule = await downloadSchedule();
+
+	await createCalendar(schedule);
+	console.log(`\n\u{2728} Successfully created calendar`);
+
+	await createPage();
+}
+
+
+async function downloadSchedule() {
+	console.time('Downloading schedule.json');
+	const result = await fetch("https://raw.githubusercontent.com/nodejs/Release/refs/heads/main/schedule.json");
+	console.timeEnd('Downloading schedule.json');
+
+	if (!result.ok) {
+		throw new Error(`Failed to download schedule: ${result.statusText}`);
+	}
+
+	return result.json();
+}
+
+async function createCalendar(schedule) {
+	const today = new Date().toISOString().split("T").at(0);
+
+	const supportedVersions = Object.fromEntries(
+		Object.entries(schedule).filter(([, release]) => {
+			return new Date(release.end).getTime() > new Date(today).getTime();
+		})
+	);
+
+	const events = Object.entries(supportedVersions).flatMap(([version, release]) => {
+		return [
+			{
+				title: `Node ${version}`,
+				start: [...release.start.split("-").map(Number), 0, 0],
+				duration: { hours: 24 },
+			},
+			release.lts ? {
+				title: `Node ${version} LTS`,
+				start: [...release.lts.split("-").map(Number), 0, 0],
+				duration: { hours: 24 },
+			} : undefined
+		];
+	}).filter(item => item);
+
+	const { error, value } = createEvents(events);
+
+	if (error) {
+		throw new Error(error.message);
+	}
+
+	console.time(`Writing ${outputFile}`);
+	await fs.writeFile(outputFile, value, 'utf-8');
+	console.timeEnd(`Writing ${outputFile}`);
+}
+
+
+async function createPage(version) {
+	console.time("Creating page");
+
+	const templateFile = resolve("./src/template.ejs");
+	const iconFile = resolve("./src/favicon.svg");
+
+	const template = (await fs.readFile(templateFile)).toString();
+	const icon = (await fs.readFile(iconFile)).toString();
+
+	const html = await htmlMinify(render(template, { version }), htmlMinifyOptions);
+	const favicon = await htmlMinify(icon, {
+		...htmlMinifyOptions,
+		removeAttributeQuotes: false,
+	});
+
+	await fs.writeFile("public/favicon.svg", favicon);
+	await fs.writeFile("public/index.html", html);
+
+	console.timeEnd("Creating page");
+}
